460add673dc60815762b0a7e3c8e7717a705de8e
RMesado <rafaelmesado@gmail.com>
2020-10-11 17:24:19 +0200

    añadido case y cambiado formulatio

--- Lab/P1/partials/registerForm.php
+++ Lab/P1/partials/registerForm.php
@@ -13,14 +13,24 @@
 		<input type="text" name="surname" class="item_requerid" size="20" maxlength="25" value="<?php print $surname ?>"
 		 placeholder="Apellido" />
 		<br/>
-		<label for="email">Email</label>
+		<label for="address">Dirección</label>
 		<br/>
-		<input type="text" name="email" class="item_requerid" size="20" maxlength="25" value="<?php print $email ?>"
-		 placeholder="kiko@ic.es" />
+		<input type="text" name="address" class="item_requerid" size="20" maxlength="25" value="<?php print $address ?>"
+		 placeholder="" />
 		<br/>
-		<label for="passwd">Clave</label>
+		<label for="city">Ciudad</label>
 		<br/>
-		<input type="password" name="passwd" class="item_requerid" size="8" maxlength="25" value="<?php print $passwd ?>"
+		<input type="text" name="city" class="item_requerid" size="8" maxlength="25" value="<?php print $city ?>"
+		/>
+		<br/>
+		<label for="zip_code">Código Postal</label>
+		<br/>
+		<input type="text" name="zip_code" class="item_requerid" size="5" maxlength="5" value="<?php print $zip_code ?>"
+		/>
+		<br/>
+		<label for="foto_file">Archivo fotográfico</label>
+		<br/>
+		<input type="text" name="foto_file" class="item_requerid" size="8" maxlength="25" value="<?php print $foto_file ?>"
 		/>
 		<br/>
 		<p><input type="submit" value="Enviar">
--- Lab/P1/portal.php
+++ Lab/P1/portal.php
@@ -49,6 +49,36 @@ switch ($action) {
             print "</table>";
         }
         break;
+    case "registrar":
+        function handler($pdo,$table)
+        {
+            $datos = $_REQUEST;
+            if (count($_REQUEST) < 3) {
+                $data["error"] = "No has rellenado el formulario correctamente";
+                return;
+            }
+            $query = "INSERT INTO     $table (name, surname, address, city, zip_code, foto_file)
+                                VALUES (?,?,?,?,?,?)";
+                            
+            echo $query;
+            try { 
+                $a=array($_REQUEST['name'], $_REQUEST['surname'],$_REQUEST['address'], $_REQUEST['city'],
+                $_REQUEST['zip_code'], $_REQUEST['foto_file'] );
+                print_r ($a);
+                $consult = $pdo->prepare($query);
+                $a=$consult->execute(array($_REQUEST['name'], $_REQUEST['surname'],$_REQUEST['address'], $_REQUEST['city'],
+                $_REQUEST['zip_code'], $_REQUEST['foto_file'] ));
+                if (1>$a)echo "InCorrecto";
+            
+            } catch (PDOExeption $e) {
+                echo ($e->getMessage());
+            }
+        }
+
+        $table = "a_cliente";
+        var_dump($_POST);
+        handler( $pdo,$table);
+        break;
     default:
         $data["error"] = "Accion No permitida";
         $central = "/holaMundo.php";
